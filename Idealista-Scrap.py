3b9e03088a4655108455f6e7c44d3786dff51b76
Teodoroav <44656894+Teodoroav@users.noreply.github.com>
2020-07-12 22:46:52 +0200

    Update Idealista-Scrap.py

--- Idealista-Scrap.py
+++ Idealista-Scrap.py
@@ -6,40 +6,19 @@ url = "https://www.idealista.com/venta-viviendas/alicante-alacant/centro/"
 
 driver = webdriver.Chrome()
 driver.get(url)
-time.sleep(5)
+time.sleep(3)
 
 
-items = len(driver.find_elements_by_class_name("item-info-container"))-1
+items = len(driver.find_elements_by_class_name("item-price.h2-simulated"))-1
 
-for item in range(items):
-    quotes = driver.find_elements_by_class_name("item-info-container")
-    for quote in quotes:
-        descripcion = quote.find_element_by_class_name('item-link ').text
-        print(descripcion)
-        
-        p = quote.find_element_by_class_name('item-price h2-simulated').text
-        print(p)
-
-        '''
-        precios = quote.find_element_by_class_name('row price-row clearfix')
-        for precio in precios:
-            p = precio.find_element_by_class_name('item-price h2-simulated').text
-            print(precio)
-        '''
-        
-        #author = quote.find_element_by_class_name('author').text
-        #new = ((quote_text,author))
-        #total.append(new)
-
-#df = pd.DataFrame(total,columns=['quote','author'])
-#df.to_csv('quoted.csv')
-driver.quit()
+#p2 = len(driver.find_elements_by_css_selector('span.item-price.h2-simulated'))
 
 
-#driver.quit()
-'''
-print("Page Content:")
-print(contenido)
+precios = driver.find_elements_by_class_name("item-price.h2-simulated")
 
-#all_links = driver.find_elements_by_tag_name('a')
-'''
+for precio in precios:
+    p = precio.text
+    p = p.replace("€","")
+    print(p)
+
+drive.close()
