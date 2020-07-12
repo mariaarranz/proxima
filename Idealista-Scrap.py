6accff1205be3f87096c71f47cd7eaddf839d6e9
mariaarranz <56388787+mariaarranz@users.noreply.github.com>
2020-07-13 00:14:41 +0200

    Update Idealista-Scrap.py

--- Idealista-Scrap.py
+++ Idealista-Scrap.py
@@ -11,7 +11,7 @@ time.sleep(5)
 items = len(driver.find_elements_by_class_name("item-info-container"))
 
 total = []
-quotes = driver.find_elements_by_class_name("item-info-container")
+quotes = driver.find_elements_by_class_name("items-container")
 for quote in quotes:
     descripcion = quote.find_element_by_class_name('item-link ').text
     print(descripcion)
@@ -20,10 +20,22 @@ for quote in quotes:
     precio = precio.replace('€', '')
     precio = precio.replace('.', '')
     print(precio)
-    
-    new = ((descripcion,precio))
+
+    detail1 = quote.find_element_by_xpath("(//span[@class='item-detail'])[1]").text
+    print(detail1)
+
+    detail2 = quote.find_element_by_xpath("(//span[@class='item-detail'])[2]").text
+    print(detail2)
+
+    detail3 = quote.find_element_by_xpath("(//span[@class='item-detail'])[3]").text
+    print(detail3)
+
+    detail4 = quote.find_element_by_xpath("(//span[@class='item-detail'])[4]").text
+    print(detail4)
+
+    new = ((descripcion,precio,detail1,detail2,detail3))
     total.append(new)
 
-df = pd.DataFrame(total,columns=['Descripcion','Precio'])
+df = pd.DataFrame(total,columns=['Descripcion','Precio','Habitaciones','Metros','Planta'])
 df.to_csv('Idealista.csv', index=False, encoding='utf-8-sig')
 driver.quit()
